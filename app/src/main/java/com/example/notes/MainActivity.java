4c0c2c2ab0e1f07a99a4cb4e8efc185ce4a1feb8
abhay007kr <aksabhay007@gmail.com>
2019-12-19 00:12:56 +0530

    splash screen, register.xml and MainActivity.xml with new logo

--- app/src/main/java/com/example/notes/MainActivity.java
+++ app/src/main/java/com/example/notes/MainActivity.java
@@ -14,6 +14,10 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        //Setting the AppTheme to display the activity
+        setTheme(R.style.AppTheme);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
